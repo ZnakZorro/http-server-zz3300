4cd0fd0c65bec7713aaa7a88085abcb52ab9eb9e
ZnakZorro <znakzorro@gmail.com>
2021-05-24 08:38:22 +0200

    1 file modified

--- expresss.js
+++ expresss.js
@@ -19,9 +19,10 @@ app.use(express.static(path.join(__dirname, 'pages')));
 
 
 
-app.get('/radio/:nr', (req, res) => {
-    console.log(req.params.nr);
-    res.send("1");
+app.get('/radio/*', (req, res) => {
+    //console.log(req.params);
+    //let nr = req.params[0];
+    res.send(1);
    
 });
 app.get('/radio2/', (req, res) => {
