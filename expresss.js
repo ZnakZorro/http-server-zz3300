ae8e8f1912ddd6affe5413d167c7e46358bc8a54
ZnakZorro <znakzorro@gmail.com>
2021-05-24 08:27:06 +0200

    Initial commit

---
+++ expresss.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const app = express();
+const exec = require('child_process');
+
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'pages')));
+
+// Send all other requests to the Angular app
+/*app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/index.html'));
+});*/
+
+
+
+app.get('/radio1/', (req, res) => {
+    console.log(11);
+    res.send("1");
+    //res.sendFile(path.join(__dirname, 'pages/index.html'));
+
+    /*exec('mpc play 1', (err, stdout, stderr) => {
+      if (err) {onsole.log(err); return; }
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    }); */   
+});
+app.get('/radio2/', (req, res) => {
+    console.log(22);
+    res.send("1");
+    //res.sendFile(path.join(__dirname, 'pages/index.html'));
+
+   /* exec('mpc play 2', (err, stdout, stderr) => {
+      if (err) {onsole.log(err); return; }
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    }); */   
+});
+app.get('/radio3', (req, res) => {
+    console.log(33);
+    res.send("1");
+    //res.sendFile(path.join(__dirname, 'pages/index.html'));
+
+   /* exec('mpc play 3', (err, stdout, stderr) => {
+      if (err) {onsole.log(err); return; }
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    });*/
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+//Set Port
+const port = process.env.PORT || '3300';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`1 Running on localhost:${port}`));
---
+++ server.js
@@ -0,0 +1,70 @@
+
+const https = require('https');
+var http = require('http');
+
+let url ='https://www.yr.no/api/v0/locations/2-3083828/forecast'; // Dąbie
+//let url ='https://www.yr.no/api/v0/locations/5-1220500/forecast'; // Szczecin 
+
+const parsowanie=(body)=>{
+  //console.log(body);
+		for (var o in body){
+			//console.log(o);
+		}
+    console.log("------------------------------");
+    let html = "";
+		for (var o in body.longIntervals){
+      let day = body.longIntervals[o];
+			//console.log(o,day);
+      
+      let czas = (new Date(day.start)).toLocaleString('pl-PL');
+      let deszcz = Math.max(day.precipitation.value,day.precipitation.min,day.precipitation.max);
+      //console.log(czas,"T=",day.temperature.value,"FL=",day.feelsLike.value,"  D="+deszcz+" mm");
+      html += '<p>T='+day.temperature.value+' F='+day.feelsLike.value+' D='+deszcz+' mm</p>\n';
+      
+		}
+    console.log(html);
+    serverStart(html);
+
+}
+
+let getYRNO=(url)=>{
+    https.get(url,(res) => {
+        let body = "";
+        res.on("data", (chunk) => {
+            body += chunk;
+        });
+        res.on("end", () => {
+            try {
+                let json = JSON.parse(body);
+                //console.log(json);
+                parsowanie(json);
+            } catch (error) {
+                console.error(error.message);
+            };
+        });
+    }).on("error", (error) => {
+        console.error(error.message);
+    });
+}
+
+
+let serverStart=(html="qwerty")=>{
+  http
+    .createServer(function(req, res) {
+      let czas = (new Date()).toLocaleString();
+      console.log(1,czas);
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(html);
+        //res.write(czas+html+' <br />yrno Hello, Node.js!'); //write a response to the client
+        //res.end(); //end the response
+    })
+    .listen(8088); //the server object listens on port 8080
+  let czas = (new Date()).toLocaleString();
+  console.log(0,czas);
+  console.log('Server running on port 8088');
+}
+
+console.log("+++++++++++++++++++++++++++++++");
+
+getYRNO(url);
