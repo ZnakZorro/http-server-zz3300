31d88c8ee49e2c38edcfa1eda33024735d9a5548
ZnakZorro <znakzorro@gmail.com>
2021-05-24 12:00:15 +0200

    1 file modified

--- index.js
+++ index.js
@@ -93,9 +93,7 @@ app.get('/volume/*', (req, res) => {
 
 
 //Set Port
-const port = process.env.PORT || '3300';
+const port = process.env.PORT || '3330';
 app.set('port', port);
-
 const server = http.createServer(app);
-
 server.listen(port, () => console.log(`1 Running on localhost:${port}`));
